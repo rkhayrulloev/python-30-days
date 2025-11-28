41e56aa3079d04636066890d8216e6b237d5d006
Rizamat Khayrulloev <111441234+rkhayrulloev@users.noreply.github.com>
2025-11-28 23:20:18 +0500

    Update day03.py

--- day03/day03.py
+++ day03/day03.py
@@ -74,7 +74,7 @@ here
 #14 Write a Python program to calculate the number of days between two dates.
 from datetime import datetime
 date1 = input('Enter first date (YYYY-MM-DD): ')
-date2 = input('Enter first date (YYYY-MM-DD): ')
+date2 = input('Enter second date (YYYY-MM-DD): ')
 d1 = datetime.strptime(date1, '%Y-%m-%d')
 d2 = datetime.strptime(date2, '%Y-%m-%d')
 delta = abs((d2 - d1).days)
