f6fa771d83d644ab2e51991f0a34915d5f83104c
Rizamat Khayrulloev <111441234+rkhayrulloev@users.noreply.github.com>
2025-11-28 11:00:20 +0500

    Update day03.py

--- day03/day03.py
+++ day03/day03.py
@@ -1,2 +1,9 @@
 # Day 3 – Python Basic Exercises 1-50
 print("Hello, bu mening 30 kunlik sayohatim!")
+# 1. Write a Python program to print "Twinkle, twinkle, little star" formatted poem
+print("Twinkle, twinkle, little star,")
+print("\tHow I wonder what you are!")
+print("\t\tUp above the world so high,")
+print("\t\tLike a diamond in the sky.")
+print("Twinkle, twinkle, little star,")
+print("\tHow I wonder what you are")
